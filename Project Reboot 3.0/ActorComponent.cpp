2043a267ba402bf61dc47ef21aff335ec584ecfb
Gray <dsicordalt192@gmail.com>
2026-05-09 18:41:52 -0400

    Update ActorComponent.cpp

--- Project Reboot 3.0/ActorComponent.cpp
+++ Project Reboot 3.0/ActorComponent.cpp
@@ -7,5 +7,6 @@ AActor* UActorComponent::GetOwner()
 	auto GetOwnerFn = FindObject<UFunction>(L"/Script/Engine.ActorComponent.GetOwner");
 	AActor* Owner;
 	this->ProcessEvent(GetOwnerFn, &Owner);
+
 	return Owner;
 }
